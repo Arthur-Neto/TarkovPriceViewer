fa2dfd2464415accb92959b578643d1ee19c7e6c
Arthur Neto <arthur.santos@printwayy.com>
2022-04-04 17:51:31 -0300

    Updating Item class

--- TarkovPriceViewer/Item.cs
+++ TarkovPriceViewer/Item.cs
@@ -2,42 +2,42 @@
 {
     public class Item
     {
-        public string NameDisplay;
-        public char[] NameCompare;
-        public string NameDisplay2;
-        public char[] NameCompare2;
+        public string? NameDisplay;
+        public char[]? NameCompare;
+        public string? NameDisplay2;
+        public char[]? NameCompare2;
         public bool IsName2 = false;
-        public string MarketAddress;
-        public string WikiAddress;
-        public string PriceLast;
-        public string PriceDay;
-        public string PriceWeek;
-        public string SellToTrader;
-        public string SellToTraderPrice;
-        public string BuyFromTrader;
-        public string BuyFromTraderPrice;
-        public string LastUpdate;
+        public string? MarketAddress;
+        public string? WikiAddress;
+        public string? PriceLast;
+        public string? PriceDay;
+        public string? PriceWeek;
+        public string? SellToTrader;
+        public string? SellToTraderPrice;
+        public string? BuyFromTrader;
+        public string? BuyFromTraderPrice;
+        public string? LastUpdate;
 
-        public string Needs;
-        public string Bartersandcrafts;
-        public string Type;
-        public string Recoil;
-        public string Accuracy;
-        public string Ergo;
+        public string? Needs;
+        public string? Bartersandcrafts;
+        public string? Type;
+        public string? Recoil;
+        public string? Accuracy;
+        public string? Ergo;
 
         public DateTime LastFetch;
-        public Ballistic Ballistic = null;
+        public Ballistic? Ballistic = null;
 
         public string[] Data()
         {
             return new string[] {
                 IsName2 ? NameDisplay2 : NameDisplay
-                , Recoil != null ? Recoil : ""
-                , Accuracy != null ? Accuracy : ""
-                , Ergo != null ? Ergo : ""
-                , PriceLast != null ? PriceLast : ""
-                , BuyFromTraderPrice != null ? BuyFromTraderPrice.Replace(" ", "").Replace(@"~", @" ≈") : ""
-                , BuyFromTrader != null ? BuyFromTrader : ""
+                , Recoil ?? string.Empty
+                , Accuracy ?? string.Empty
+                , Ergo ?? string.Empty
+                , PriceLast ?? string.Empty
+                , BuyFromTraderPrice != null ? BuyFromTraderPrice.Replace(" ", string.Empty).Replace(@"~", @" ≈") : string.Empty
+                , BuyFromTrader ?? string.Empty
             };
         }
     }
